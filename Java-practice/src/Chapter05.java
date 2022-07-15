be535fff24b3bac17484d6e433d1f8ba347ff9f1
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-07-15 17:49:17 +0900

    [add]配列練習

--- Java-practice/src/Chapter05.java
+++ Java-practice/src/Chapter05.java
@@ -20,15 +20,18 @@ public class Chapter05 {
 		array[2] = 3;
 		array[3] = 4;
 		array[4] = 5;
-		System.out.println(array[0]);
+		System.out.println(array[2]);
 		
-		int[] array1 = new int[3];  // 配列の宣言と生成
+		int[] array1 = { 0, 10, 20, 30, 40 };
+        System.out.println(array1[3]);
+		
+		int[] array2 = new int[3];  // 配列の宣言と生成
 		array1[0] = 60;  // インデックス番号「0」を指定して１番目の要素にデータを代入
 		array1[2] = 20;  // ２番目の要素より先に３番目の要素にデータを代入
 		array1[1] = 20;  // ３番目と同じ値を２番目の要素に代入
-		int int2 = array1[2];  // インデックス番号「2」を指定して３番目の要素を取得
+		int int2 = array2[2];  // インデックス番号「2」を指定して３番目の要素を取得
 		System.out.println(int2);
-		int array2[] = null;  // []は変数名の後ろでも可
-		System.out.println(array2);	
+		int array3[] = null;  // []は変数名の後ろでも可
+		System.out.println(array3);	
 	}
 }	
