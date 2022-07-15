c57e221b4704aa656be0836ab9f13515438e703e
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-07-15 13:47:25 +0900

    [Finish]４章

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,23 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+		String str1 = "今井理沙";
+		System.out.println(str1);
+		str1 = "今井理沙(改)";
+		System.out.println(str1);
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+		
+		/*
+		カリキュラム模範解答
+		final int LUCKY_NUMBER = 7;
+		String str ="ウェブ　キャンプ之助";
+		System.out.println(str);
+		str = "少年よ大志を抱け";
+		System.out.println(str);
+  		System.out.println(LUCKY_NUMBER);
+		*/
+	}
+
+}
---
+++ JavaWcp/src/Variable01.java
@@ -0,0 +1,20 @@
+
+public class Variable01 {
+	
+	public static void main(String[] args) {
+//		定義の仕方 型 変数名;
+		int num1; // int型の変数num1を宣言
+		String str1; // string型の変数str1を宣言
+		num1 = 10; // num1を初期化
+		str1 = "Hello World!";
+		System.out.println(num1);
+		System.out.println(str1);
+		
+		num1 = 20; // num1に20を再代入
+		System.out.println(num1);
+		
+		int num2 = 30; // int型の変数num2を宣言と同時に初期化
+		System.out.println(num2);
+	}
+
+}
