8a0077044de5ef40154985a0b04e577856b3f67d
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-07-18 13:09:00 +0900

    [Finish]８章

--- Java-practice/src/For02.java
+++ Java-practice/src/For02.java
@@ -1,7 +1,7 @@
 
 public class For02 {
 	public static void main(String[] args) {
-		int[] array = { 1, 2, 3, 4, 5 };
+		int[] array = { 1, 3, 5, 7, 9 };
 		for (int number : array) {
 			System.out.println("For02 = " + number);
 		}
