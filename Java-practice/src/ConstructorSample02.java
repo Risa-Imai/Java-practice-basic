d608d6dfe8d92734bf4a20a7ddbe0bae1a807afa
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-07-20 15:40:02 +0900

    [途中経過]１０章

--- Java-practice/src/ConstructorSample02.java
+++ Java-practice/src/ConstructorSample02.java
@@ -1,4 +1,16 @@
 
-public class ConstructorSample02 {
-
+class ConstructorSample02 {
+	String name;
+	
+	ConstructorSample02(String str) {
+		name = str;
+	}
+	
+	public static void main(String[] args) {
+		ConstructorSample02 sample01 = new ConstructorSample02("sample");
+		ConstructorSample02 sample02 = new ConstructorSample02("サンプル");
+		
+		System.out.println(sample01.name);
+		System.out.println(sample02.name);
+	}
 }
