4a635d7e120c4e945fe79c1c7f5eaf933a195e2b
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-07-20 18:43:04 +0900

    [Finish]10章(理解度10%)

--- Java-practice/src/exception/Exception03.java
+++ Java-practice/src/exception/Exception03.java
@@ -3,22 +3,29 @@ package exception;
 public class Exception03 {
 	public static void main(String[] args) {
 		try {
+			// sivishionメソッドを呼び出し
 			Exception03.division(100, 0);
+		// ArithmeticExceptionクラスの例外が発生した場合の例外処理
 		} catch (ArithmeticException e) {
+			// 例外クラスが一致しないので、例外処理が実行されない
 			System.out.println("ArithmeticException例外が発生");
+		// IllegalArgumentExceptionクラスの例外が発生した場合の例外処理
 		} catch (IllegalArgumentException e) {
+			// 例外処理が実行される
 			System.out.println("IllegaArgumentException例外が発生");
+			// キャッチした例外インスタンスをスロー
 			throw e;
 		}
-		System.out.println("ブログ欄修了");
+		// finallyブロックでないので処理が実行されない
+		System.out.println("プログラム終了");
 	}
 	
 	public static void division(int a, int b) {
 		System.out.println(a + " ÷ " + b + " は？");
 		if (b == 0) {
+			// 割り算で例外が発生するので、意図的にIllegalArgumentException例外をスロー
 			throw new IllegalArgumentException("引数が不正です");
 		}
-		
 		int result = a / b;
 		System.out.println("計算結果" + result);
 	}
