13bff74b53485d1359ea83c40a8f28b09c39884d
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-07-27 00:50:03 +0900

    修正

--- Java-practice/src/kuhu.java
+++ Java-practice/src/kuhu.java
@@ -3,11 +3,9 @@ public class kuhu {
 	public static void main(String[] args) {
 		int i = 10;
 		String square = "■";
-		String squ = square * i;
 		
 		while(i >= 0) {
-			String squ = square * i;
-			System.out.println(squ);
+			System.out.println(square);
 			i--;
 		}
 	}
