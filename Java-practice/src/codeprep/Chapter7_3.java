117852b194fe3dd44e2cccace6c22110c0c2aed1
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-01 01:07:09 +0900

    メソッド化練習

--- Java-practice/src/codeprep/Chapter7_3.java
+++ Java-practice/src/codeprep/Chapter7_3.java
@@ -1,5 +1,20 @@
 package codeprep;
 
-public class Chapter7_3 {
+import java.util.Scanner;
 
+public class Chapter7_3 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int i = scanner.nextInt();
+		putMethod(i);
+
+		}
+	
+		public static void putMethod(int i) {
+			while (i < 10) {
+			System.out.println(i);
+			i += 2;			
+		}
+	}
 }
