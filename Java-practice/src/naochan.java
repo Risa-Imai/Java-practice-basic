eb67ee72364fd6eb73ddf371fc752c3a123f57d5
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-02 00:45:09 +0900

    コード練習

---
+++ Java-practice/src/naochan.java
@@ -0,0 +1,16 @@
+//
+//public class naochan {
+//	public static void main(String[] args) {
+//		double x = 3;
+//		double y = 7;
+//		double z = subPlus(x, y);
+//		double res = subInverse(z);
+//		System.out.println(subPlus(c));
+//	}
+//	public static double subPlus(a, b) {
+//		(a + b);
+//	}
+//	public static double subInverse(c) {
+//		(1 / c);
+//	}
+//}
