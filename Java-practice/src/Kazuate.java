3e5bc7ea56abf850f6e537171716287bfbb68ff6
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-06 00:52:36 +0900

    数あてゲーム

--- Java-practice/src/Kazuate.java
+++ Java-practice/src/Kazuate.java
@@ -5,8 +5,21 @@ public class Kazuate {
 	public static void main(String[] args) {
 		// 入力準備
 		Scanner sc = new Scanner(System.in);
-		// コンピュータ
-
+		// コンピュータが数を選ぶ
+		int a = (int)(Math.random() * 20) + 1;
+		// 数を入力
+		System.out.println("当ててごらん：");
+		int x = sc.nextInt();
+		// 当たるまで繰り返し
+		while ( x != a) {
+			// はずれのメッセージ
+			System.out.println("違うよーん");
+			// 数を入力
+			System.out.println("当ててごらん");
+			x = sc.nextInt();
+		}
+		// 最後のメッセージ
+		System.out.println("当たりです");
 	}
 
 }
