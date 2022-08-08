7e7018086aa796ae97bb9011ae2abcba52e3c072
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-08 12:42:24 +0900

    [Add]Code1_3コメント追加

--- Java-practice/src/sukkiri/Code1_3.java
+++ Java-practice/src/sukkiri/Code1_3.java
@@ -2,9 +2,9 @@ package sukkiri;
 
 public class Code1_3 {
 	public static void main(String[] args) {
-		int age = 20;
+		int age = 20; // 変数ageを20で初期化
 		System.out.println("私の年齢は" + age);
-		age = 31;
+		age = 31; // 変数ageを再代入
 		System.out.println("…いや、本当の年齢は" + age);
 	}
 }
