8817f6a9ad9a3cd525b5ebb1238ec49292c3d8de
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-08 13:58:12 +0900

    [Fix]練習問題1_2リファクタリング

--- Java-practice/src/sukkiri/practice1_2.java
+++ Java-practice/src/sukkiri/practice1_2.java
@@ -4,6 +4,7 @@ public class practice1_2 {
 	public static void main(String[] args) {
 		int a = 3;
 		int b = 5;
-		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + a * b);
+		int c = a * b;
+		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
 	}
 }
