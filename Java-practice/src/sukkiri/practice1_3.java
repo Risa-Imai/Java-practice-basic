0fffe86e8303182eb78dd2cd40d7c1707fe66482
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-08 15:01:09 +0900

    [Finish]practice1_1

---
+++ Java-practice/src/sukkiri/practice1_3.java
@@ -0,0 +1,12 @@
+package sukkiri;
+
+public class practice1_3 {
+	public static void main(String[] args) {
+		boolean bool = true;
+		char name = '駆';
+		double a = 3.14;
+		float b = 3.14f;
+		long c = 314159265853979L;
+		System.out.println("ミナトの攻撃！敵に15ポイントのダメージを与えた。");
+	}
+}
