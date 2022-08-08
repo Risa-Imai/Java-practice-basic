63834263736fdde8545dfa82b2fc9d2b3312fb89
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-08 15:07:15 +0900

    リファクタリング

--- Java-practice/src/sukkiri/practice1_3.java
+++ Java-practice/src/sukkiri/practice1_3.java
@@ -2,11 +2,11 @@ package sukkiri;
 
 public class practice1_3 {
 	public static void main(String[] args) {
-		boolean bool = true;
-		char name = '駆';
-		double a = 3.14;
-		float b = 3.14f;
-		long c = 314159265853979L;
+		boolean result = true;
+		char favoriteCharacter = '駆';
+		double pi = 3.14;
+		final float PI = 3.14f;
+		long nuber = 314159265853979L;
 		System.out.println("ミナトの攻撃！敵に15ポイントのダメージを与えた。");
 	}
 }
