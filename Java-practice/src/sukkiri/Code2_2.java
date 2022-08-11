4a315baa1c821a010ebebcbf9b10a7ad4d1b9627
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-11 15:34:49 +0900

    エスケープシーケンス・引用符記号について

--- Java-practice/src/sukkiri/Code2_2.java
+++ Java-practice/src/sukkiri/Code2_2.java
@@ -3,7 +3,9 @@ package sukkiri;
 public class Code2_2 {
 
 	public static void main(String[] args) {
-		System.out.println("私の好きな記号は二重引用符(\")です");
+		/* コンパイルエラー
+		  System.out.println("私の好きな記号は二重引用符(")です");
+		 */
 	}
 
 }
