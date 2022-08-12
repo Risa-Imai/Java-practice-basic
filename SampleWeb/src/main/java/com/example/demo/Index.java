a7ce7055ccf42e1706a30b69899c8cdacacca3d6
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-12 23:56:29 +0900

    [Add]index

---
+++ SampleWeb/src/main/java/com/example/demo/Index.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Index {
+	@RequestMapping("/")
+	public String start() {
+		return "Index.html";
+	}
+}
