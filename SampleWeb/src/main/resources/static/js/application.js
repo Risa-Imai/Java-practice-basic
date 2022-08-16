956d9e2c37e2bb293656238b26e7dbed6e0af654
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-17 01:03:41 +0900

    途中

--- SampleWeb/src/main/resources/static/js/application.js
+++ SampleWeb/src/main/resources/static/js/application.js
@@ -4,3 +4,9 @@
 $('.openbtn4').click(function () {
     $(this).toggleClass('active');
 });
+
+$(function(){
+  $('openbtn4').on('click', function(){
+    $('.menu').toggleClass('is-active');
+  });
+}());
