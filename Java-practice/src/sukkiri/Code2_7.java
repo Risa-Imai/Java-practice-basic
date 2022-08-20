cc4acf331cc1096b1607a535996dd83edde13c56
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-20 16:29:59 +0900

    キャスト演算子

--- Java-practice/src/sukkiri/Code2_7.java
+++ Java-practice/src/sukkiri/Code2_7.java
@@ -4,5 +4,7 @@ public class Code2_7 {
 	public static void main(String[] args) {
 		int age = (int)3.2; //3.2をintに型変換して代入せよ！(強制的な型変換)
 		System.out.println(age);
+		// 3.2というdouble型リテラルの前に記述された「(int)」が強制的な型変換を指示する
+		// キャスト演算子(cast operator)
 	}
 }
