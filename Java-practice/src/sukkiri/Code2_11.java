1fa919e36f90430b442bd10f26dcacb9acd00879
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-22 22:20:58 +0900

    [Add]2_12

---
+++ Java-practice/src/sukkiri/Code2_11.java
@@ -0,0 +1,11 @@
+package sukkiri;
+
+public class Code2_11 {
+	public static void main(String[] args) {
+		String name = "すがわら";
+		System.out.print("私の名前は");
+		System.out.print(name);
+		System.out.print("です");
+		// System,out.printは改行せずに画面に文字を表示する
+	}
+}
