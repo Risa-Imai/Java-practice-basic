1a14e05d3e3d233333de0bb34426f85e3ee76ff4
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-22 22:06:12 +0900

    [Add]Code2_9

---
+++ Java-practice/src/sukkiri/Code2_9.java
@@ -0,0 +1,9 @@
+package sukkiri;
+
+public class Code2_9 {
+	public static void main(String[] args) {
+		String msg = "私の年齢は" + 23;
+		System.out.println(msg);
+		// 23(int型)が"23"(String型)に変換されて連結される
+	}
+}
