d1a21723510e9da3d5041a7ce564b6fb1a74520d
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-23 14:44:01 +0900

    [Add]Code2_13

---
+++ Java-practice/src/sukkiri/Code2_13.java
@@ -0,0 +1,12 @@
+package sukkiri;
+
+// 文字列を数値に変換する
+// int n = Integer.parseInt(①);
+public class Code2_13 {
+	public static void main(String[] args) {
+		String age = "31";
+		int n = Integer.parseInt(age);
+		System.out.println
+			("あなたは来年、" + (n + 1) + "歳になりますね。");
+	}
+}
