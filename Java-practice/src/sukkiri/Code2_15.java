2fe80f1329c571b91bb97c55430dcaca0ede2780
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-23 15:08:45 +0900

    [Add]Code2_15注釈追加

--- Java-practice/src/sukkiri/Code2_15.java
+++ Java-practice/src/sukkiri/Code2_15.java
@@ -4,8 +4,10 @@ package sukkiri;
 public class Code2_15 {
 	public static void main(String[] args) {
 		System.out.println("あなたの名前を入力してください。");
+		// キーボードから１行の文字列の入力を受け付ける
 		String name = new java.util.Scanner(System.in).nextLine();
 		System.out.println("あなたの年齢を入力してください。");
+		// キーボードから１つの整数の入力を受け付ける
 		int age = new java.util.Scanner(System.in).nextInt();
 		System.out.println("ようこそ、" + age + "歳の" + name + "さん");
 	}
