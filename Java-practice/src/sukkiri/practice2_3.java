1d98be58eeb1feaaa976249ddb301dc9e25a30c8
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-25 16:03:07 +0900

    [Add]答え合わせ

--- Java-practice/src/sukkiri/practice2_3.java
+++ Java-practice/src/sukkiri/practice2_3.java
@@ -8,8 +8,8 @@ public class practice2_3 {
 		System.out.println("あなたの年齢を入力してください");
 		String ageString = new java.util.Scanner(System.in).nextLine();
 		int age = Integer.parseInt(ageString);
-		int fortune = new java.util.Random().nextInt(3);
-		fortune += 1;
+		int fortune = new java.util.Random().nextInt(4);
+		fortune += 1; // fortune++; 要件はインクリメント
 		System.out.println("占いの結果が出ました！");
 		System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
 		System.out.println("1:大吉 2:中吉 3:吉 4:凶");		
