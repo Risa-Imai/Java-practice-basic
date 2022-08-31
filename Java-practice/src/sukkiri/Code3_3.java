d4f66824ee1f64bf40ffb125f3f57a98f8452d52
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-31 20:01:17 +0900

    [Add]Code3_3あまり推奨されないブロックの波カッコ省略

---
+++ Java-practice/src/sukkiri/Code3_3.java
@@ -0,0 +1,15 @@
+package sukkiri;
+
+public class Code3_3 {
+
+	public static void main(String[] args) {
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩にいきます");
+		} else
+			System.out.println("DVDを見ます");
+
+	}
+
+}
