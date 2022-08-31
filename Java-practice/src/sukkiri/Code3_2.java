f8b376404c29b24374441530c96b9a84a4761ce5
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-01 00:52:37 +0900

    [add]

---
+++ Java-practice/src/sukkiri/Code3_2.java
@@ -0,0 +1,12 @@
+package sukkiri;
+
+public class Code3_2 {
+	public static void main(String[] args) {
+		boolean doorClose = true; // ここでtrueかfalseを代入
+		while (doorClose == true) {
+			System.out.println("ノックする");
+			System.out.println("１分待つ");
+		// whileは無限ループ
+		}
+	}
+}
--- Java-practice/src/sukkiri/Code3_3.java
+++ Java-practice/src/sukkiri/Code3_3.java
@@ -4,10 +4,10 @@ public class Code3_3 {
 
 	public static void main(String[] args) {
 		boolean tenki = true;
-		if (tenki == true) {
+		if (tenki == true) { // 内容が２行なので波カッコは省略可能
 			System.out.println("洗濯をします");
 			System.out.println("散歩にいきます");
-		} else
+		} else // １行しかないので波カッコは省略可能
 			System.out.println("DVDを見ます");
 
 	}
