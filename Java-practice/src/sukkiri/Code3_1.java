d2765ccaf31ecbdfb5639eb5ead1e10cb8f36248
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-08-31 19:44:48 +0900

    [Add]Code3_1

---
+++ Java-practice/src/sukkiri/Code3_1.java
@@ -0,0 +1,16 @@
+package sukkiri;
+
+public class Code3_1 {
+
+	public static void main(String[] args) {
+		boolean tenki = true;
+		// もし変数tenkiがtrueだったら
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩にいきます");
+		} else {
+			System.out.println("DVDを見ます");
+		}
+	}
+
+}
