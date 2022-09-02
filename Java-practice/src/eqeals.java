f727a12f73a8193794836683e4767a19c5540e3a
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-02 12:07:29 +0900

    Java演習問題

---
+++ Java-practice/src/eqeals.java
@@ -0,0 +1,12 @@
+
+public class eqeals {
+	public static void main(String[] args) {
+		String str = "夕";
+		
+		if (str.equals("夕日")) {
+			System.out.println("綺麗な夕日ですね");
+		} else {
+			System.out.println("雲が出ているので夕日は見えません");
+		}
+	}
+}
