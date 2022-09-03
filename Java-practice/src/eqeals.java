7c63661d20162c3f216f43305d2c760bc8ad989a
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-03 21:10:24 +0900

    [Add]

--- Java-practice/src/eqeals.java
+++ Java-practice/src/eqeals.java
@@ -1,9 +1,10 @@
 
 public class eqeals {
 	public static void main(String[] args) {
-		String str = "夕";
+		String str = "夕日";
 		
 		if (str.equals("夕日")) {
+			// Javaでは文字列を比較する時、str == "夕日"だとタチの悪い不具合が起きる
 			System.out.println("綺麗な夕日ですね");
 		} else {
 			System.out.println("雲が出ているので夕日は見えません");
