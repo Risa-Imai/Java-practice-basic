85bc5812aed054b2dad89fcd285579a328669182
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-05 21:46:37 +0900

    繰り返し

---
+++ Java-practice/src/for_for.java
@@ -0,0 +1,12 @@
+
+public class for_for {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 3; i++) {
+			for(int j = 3; j >= 1; j--) {
+				if ( i != j) {
+					System.out.println("(" + i + "," + j + ")");
+				}
+			}
+		}
+	}
+}
