46df8dd786061d16e54c62f19e9f4d5685f02eba
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-06 13:15:10 +0900

    [Add]Code3_5

---
+++ Java-practice/src/sukkiri/Code3_6.java
@@ -0,0 +1,22 @@
+package sukkiri;
+
+// あえてbreak文を書かない
+public class Code3_6 {
+	public static void main(String[] args) {
+		System.out.println("あなたの運勢を占います");
+		int fortune = new java.util.Random().nextInt(5)+1;
+		switch(fortune) {
+		case 1:
+		case 2:
+			System.out.println("いいね！");
+			break;
+		case 3:
+		case 4:
+			System.out.println("普通です");
+			break;
+		case 5:
+		case 6:
+			System.out.println("うーん...");
+		}
+	}
+}
