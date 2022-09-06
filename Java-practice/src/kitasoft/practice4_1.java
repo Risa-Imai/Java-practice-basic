c34956f9b4763472afff83ff8930140de1356d33
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-06 13:20:56 +0900

    for文

--- Java-practice/src/kitasoft/practice4_1.java
+++ Java-practice/src/kitasoft/practice4_1.java
@@ -1,11 +1,16 @@
 package kitasoft;
 
+import java.io.*;
+
 public class practice4_1 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int i = 1;
 		while(i <= 10) {
 			System.out.println(i + ":SPAM");
 			i++;
+		}
+		for(int j = 0; j < 10; j++) {
+			System.out.println(j + ":SPAMEX");
 		}
 	}
 }
