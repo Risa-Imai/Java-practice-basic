2249aaa99ed73c67b0769f11aac4218e55c070b9
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-08 10:19:18 +0900

    [Add]Code3_8

---
+++ Java-practice/src/sukkiri/Code3_8.java
@@ -0,0 +1,11 @@
+package sukkiri;
+
+import java.io.*;
+
+public class Code3_8 {
+	public static void main(String[] args)throws IOException{
+		for (int i = 0; i < 3; i++) {
+			System.out.print("現在" + (i + 1) + "周目⇒");
+		}
+	}
+}
