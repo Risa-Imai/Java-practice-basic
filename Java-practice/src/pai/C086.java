28d0105cea027651191953b07591cac31ebb26ff
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-09 14:24:40 +0900

    c086

---
+++ Java-practice/src/pai/C086.java
@@ -0,0 +1,13 @@
+package pai;
+
+import java.io.*;
+import java.util.*;
+
+public class C086 {
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		String result = s.replaceAll("[AIUEOaiueo]", "");
+		System.out.println(result);
+	}
+}
