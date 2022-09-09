3f2e3c50d0bb0609e153a144d4634f18c22e46ad
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-09 14:31:58 +0900

    d222

---
+++ Java-practice/src/pai/D222.java
@@ -0,0 +1,13 @@
+package pai;
+
+import java.util.*;
+import java.io.*;
+
+public class D222 {
+	public static void main(String[] args)throws IOException{
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		System.out.println(Math.max(x, y));
+	}
+}
