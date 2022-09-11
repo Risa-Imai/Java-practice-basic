1b8266e1e247622c03b0ac0c5c86508d5cd0c86f
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-11 16:45:04 +0900

    fix

--- Java-practice/src/sukkiri/practice3_3.java
+++ Java-practice/src/sukkiri/practice3_3.java
@@ -14,8 +14,10 @@ public class practice3_3 {
 			System.out.println("お腹がいっぱいです");
 		}else {
 			System.out.println("はらぺこです");
-			System.out.println(food + "をいただきます");
-			System.out.println("ごちそうさまでした");
 		}
+		if(isHungry == 1) {
+			System.out.println(food + "をいただきます");
+		}
+		System.out.println("ごちそうさまでした");
 	}
 }
