ce57e779a6f9de14c91e6a0c0d3eb9406eae7da2
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-11 16:40:57 +0900

    add

---
+++ Java-practice/src/sukkiri/practice3_3.java
@@ -0,0 +1,21 @@
+package sukkiri;
+
+import java.io.*;
+import java.util.*;
+
+public class practice3_3 {
+	public static void main(String[] args)throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int isHungry = 1;
+		System.out.print("好きな食べ物を入力してください:");
+		String food = sc.nextLine();
+		System.out.println("こんにちは");
+		if(isHungry == 0) {
+			System.out.println("お腹がいっぱいです");
+		}else {
+			System.out.println("はらぺこです");
+			System.out.println(food + "をいただきます");
+			System.out.println("ごちそうさまでした");
+		}
+	}
+}
