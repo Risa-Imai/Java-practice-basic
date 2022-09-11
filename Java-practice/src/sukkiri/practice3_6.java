ee7837fe7577b85ffaaacd49810d40e61eedcc02
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-11 17:11:01 +0900

    fix

--- Java-practice/src/sukkiri/practice3_6.java
+++ Java-practice/src/sukkiri/practice3_6.java
@@ -5,9 +5,9 @@ import java.io.*;
 
 public class practice3_6 {
 	public static void main(String[] args)throws IOException{
-		System.out.println("【数あてゲーム】");
+		System.out.print("【数あてゲーム】");
 		int ans = new Random().nextInt(8);
-		for(int i = 0; i <= 4; i++) {
+		for(int i = 0; i < 5; i++) {
 			System.out.println("数字を入力してください");
 			int num = new Scanner(System.in).nextInt();
 			if(num == ans) {
