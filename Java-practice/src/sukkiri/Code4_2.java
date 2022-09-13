f4097c3d6a11f05adc709c92a2e65ce2cb2fc96f
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-13 12:09:09 +0900

    [Add]Code4_2

---
+++ Java-practice/src/sukkiri/Code4_2.java
@@ -0,0 +1,10 @@
+package sukkiri;
+
+public class Code4_2 {
+	public static void main(String[] args) {
+		int[] scores;
+		scores = new int[5];
+		// int [] scores = new int[5];
+		// 1行でも記述可能
+	}
+}
