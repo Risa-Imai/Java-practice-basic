222cb1589f0f77308dbabef9dd466046f0b2580d
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-09-13 14:18:21 +0900

    [Add]Code4_10

---
+++ Java-practice/src/sukkiri/Code4_10.java
@@ -0,0 +1,14 @@
+package sukkiri;
+
+public class Code4_10 {
+	public static void main(String[] args) {
+		int[] scores = {20, 30, 40, 50, 80};
+		int sum = 0; // 集計結果を入れるための変数を初期化して準備
+		for (int i = 0; i < scores.length; i++) {
+			sum += scores[i]; // １科目ずつ変数sumに合算する
+		}
+		int avg = sum / scores.length;
+		System.out.println("合計点：" + sum);
+		System.out.println("平均点：" + avg);
+	}
+}
