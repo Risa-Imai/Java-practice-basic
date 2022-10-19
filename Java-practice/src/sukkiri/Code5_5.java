b9f346f4e4cf428a16b07f56c25f59be96ee2fce
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-10-19 23:18:47 +0900

    [Fix]Code5_5

---
+++ Java-practice/src/sukkiri/Code5_5.java
@@ -0,0 +1,17 @@
+package sukkiri;
+
+public class Code5_5 {
+	public static void main(String[] args) {
+		int x = new java.util.Scanner(System.in).nextInt();
+		int y = new java.util.Scanner(System.in).nextInt();
+		add(x, y);
+		add(100, 20);
+		add(200, 50);
+	}
+	
+	// 複数の値を受け取るaaddメソッド
+	public static void add(int x,int y) {
+		int ans = x + y;
+		System.out.println(x + "+" + y + "=" + ans);
+	}
+}
