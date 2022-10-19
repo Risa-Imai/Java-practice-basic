8df247669dbcb1c61b798162e6ff0f235b37f0b6
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-10-19 15:50:06 +0900

    [Add]practice4_2 拡張for文

--- Java-practice/src/sukkiri/practice4_2.java
+++ Java-practice/src/sukkiri/practice4_2.java
@@ -7,5 +7,9 @@ public class practice4_2 {
 		for (int i = 0; i < moneyList.length; i++) {
 			System.out.println(moneyList[i]);
 		}
+		// 拡張for文
+		for (int m : moneyList) {
+			System.out.println(m);
+		}
 	}
 }
