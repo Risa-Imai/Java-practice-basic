d8d0132ceefee7f7a1132c7899a3b2487e333a69
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-10-19 15:38:01 +0900

    [Add]Code4_18

--- Java-practice/src/sukkiri/Code4_16.java
+++ Java-practice/src/sukkiri/Code4_16.java
@@ -1,5 +1,14 @@
 package sukkiri;
 
 public class Code4_16 {
-
+	public static void main(String[] args) {
+		boolean judge = true;
+		if (judge == true) {
+			int[] array = {1, 2, 3};
+		}
+	}
 }
+// ガベージコレクション
+// ブロック変数の中で作成された配列はブロック外では使用する事が出来ないが
+// メモリとしては残り続ける
+// Javaはガベージコレクションを使って使用していないメモリは自動的に削除してくれる機能が備わっている
--- Java-practice/src/sukkiri/Code4_17.java
+++ Java-practice/src/sukkiri/Code4_17.java
@@ -6,6 +6,7 @@ public class Code4_17 {
 	public static void main(String[] args) {
 		int[] array = {1, 2, 3};
 		array = null;
+		// nullにした変数には代入できない
 		array[0] = 10;
 	}
 }
