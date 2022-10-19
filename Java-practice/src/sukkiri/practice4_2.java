52632167bf244dc01bcf16b2334736fa6b9f484d
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-10-19 15:48:44 +0900

    [Add]practice4_2

---
+++ Java-practice/src/sukkiri/practice4_2.java
@@ -0,0 +1,11 @@
+package sukkiri;
+
+public class practice4_2 {
+	public static void main(String[] args) {
+		int[] moneyList = {121902, 8302, 55100};
+		// for文
+		for (int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+		}
+	}
+}
