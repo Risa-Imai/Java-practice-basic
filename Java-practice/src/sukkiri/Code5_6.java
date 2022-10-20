094be9462e4f8cb860a9b2c8bd8e6acd66efdcff
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-10-20 19:29:43 +0900

    [Add]Code5_7

--- Java-practice/src/sukkiri/Code5_6.java
+++ Java-practice/src/sukkiri/Code5_6.java
@@ -3,13 +3,13 @@ package sukkiri;
 
 // コンパイルエラーなる
 public class Code5_6 {
-	public static void main(String[] args) {
-		int x = 100;
-		int y = 10;
-		add();
-	}
-	public static void add() {
-		int ans = x + y;
-		System.out.println(x + "+" + y + "=" + ans);
-	}
+//	public static void main(String[] args) {
+//		int x = 100;
+//		int y = 10;
+//		add();
+//	}
+//	public static void add() {
+//		int ans = x + y;
+//		System.out.println(x + "+" + y + "=" + ans);
+//	}
 }
