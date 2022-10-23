d5f84305c186ba51813246e272222eb756ead97b
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-10-23 21:01:40 +0900

    [Add]practice5_1

---
+++ Java-practice/src/sukkiri/practice5_1.java
@@ -0,0 +1,17 @@
+package sukkiri;
+
+public class practice5_1 {
+	public static void introduceOneself() {
+		String name = "今井理沙";
+		int age = 29;
+		double height = 163.3;
+		char eto = '酉';
+		System.out.println("名前は" + name);
+		System.out.println("年齢は" + age + "歳");
+		System.out.println("身長は" + height + "cm");
+		System.out.println("干支は" + eto);
+	}
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+}
