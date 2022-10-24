ec198551f2d0d7f7c6e80463fcb464766507936e
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-10-24 18:12:22 +0900

    [Add]practice6_1

---
+++ Java-practice/src/kitasoft/practice6_1.java
@@ -0,0 +1,12 @@
+package kitasoft;
+
+public class practice6_1 {
+	public static int squared(int num) {
+		int result = num * num;
+		return result;
+	}
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.println(squared(num));
+	}
+}
--- Java-practice/src/sukkiri/practice5_4.java
+++ Java-practice/src/sukkiri/practice5_4.java
@@ -1,5 +1,21 @@
 package sukkiri;
 
 public class practice5_4 {
-
+	public static double calcTriangleArea(double bottom, double height) {
+		double area = (bottom * height) / 2.0;
+		return area;
+	}
+	public static double calcCircleArea(double radius) {
+		double area = radius * radius * 3.14;
+		return area;
+	}
+	public static void main(String[] args) {
+		double bottom = 10.0;
+		double height = 5.0;
+		double radius = 5.0;
+		double triangleArea = calcTriangleArea(bottom, height);
+		System.out.println("三角形の面積：" + triangleArea + "平方cm");
+		double circleArea = calcCircleArea(radius);
+		System.out.println("円の面積：" + circleArea + "平方cm");		
+	}
 }
