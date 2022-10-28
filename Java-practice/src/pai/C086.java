c24ce8f3e717f958fa4a1decd71770a6375d42e3
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-10-28 23:23:08 +0900

    [Fix}

--- Java-practice/src/pai/C086.java
+++ Java-practice/src/pai/C086.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class C086 {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
 		String result = s.replaceAll("[AIUEOaiueo]", "");
