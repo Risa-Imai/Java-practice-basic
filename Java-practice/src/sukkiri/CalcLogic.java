824858cc7b4e765045562db6045980a04c3f88ac
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-10-31 23:13:21 +0900

    Fix

--- Java-practice/src/sukkiri/CalcLogic.java
+++ Java-practice/src/sukkiri/CalcLogic.java
@@ -1,5 +1,12 @@
 package sukkiri;
 
 public class CalcLogic {
-
+	public static int tasu(int a, int b) {
+		int plus = a + b;
+		return plus;
+	}
+	public static int hiku(int a, int b) {
+		int minus = a = b;
+		return minus;
+	}
 }
